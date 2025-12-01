95b15709b736c59e710367d5599ccb1d91397d1c
pniyongabo <patrickniyongabo@gmail.com>
2025-12-01 11:43:48 -0800

    2025 - day 1 - part 1

---
+++ 2025/1.py
@@ -0,0 +1,36 @@
+# Open the file in read mode
+file = open("1.txt", "r")
+
+# Read the entire content of the file
+# content = file.read()
+# print(content)
+
+# Reading a file line by line
+num_lines = 0
+position = 50
+password = 0
+
+for line in file:
+    num_lines += 1
+    direction = line[0]
+    length = int(line[1:])
+
+    if direction == "L":
+        position = position - length
+    elif direction == "R":
+        position = position + length
+
+    if position > 99 or position < 0:
+        position = position % 100
+
+    if position == 0:
+        password += 1
+
+    # print(f"The dial now points at {position}")
+
+print(f"num_lines: {num_lines}")
+print(f"final password: {password}")
+
+
+# Close the file
+file.close()
