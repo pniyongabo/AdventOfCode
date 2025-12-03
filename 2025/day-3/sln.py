ec6095ddca44a679c0b0dd7a65340897363a7e31
pniyongabo <patrickniyongabo@gmail.com>
2025-12-03 12:10:32 -0800

    2025 - day 3 - part 2

--- 2025/day-3/sln.py
+++ 2025/day-3/sln.py
@@ -1,3 +1,6 @@
+from itertools import combinations
+
+
 def find_max_joltage_bank_part_1(bank):
     """
     Find the maximum joltage possible from a single bank.
@@ -16,14 +19,43 @@ def find_max_joltage_bank_part_1(bank):
     return max_joltage
 
 
+def find_max_joltage_bank_part_2_brute_force(bank):
+    """
+    Find the maximum joltage possible from a single bank.
+    Pick exactly 12 digits from the bank (maintaining order) to form
+    the largest possible 12-digit number.
+    """
+    max_joltage = 0
+
+    # Try all possible combinations of 12 positions
+    for positions in combinations(range(len(bank)), 12):
+        # Form a 12-digit number from the selected positions
+        joltage_str = ''.join(bank[i] for i in positions)
+        joltage = int(joltage_str)
+        max_joltage = max(max_joltage, joltage)
+
+    return max_joltage
+
+
+def find_max_joltage_bank_part_2_optimized(bank):
+    max_total_joltage = bank[:12]
+    for i in range(12, len(bank)):
+        current_joltage = bank[i]
+        for i in range(12):
+            temp = max_total_joltage[0:i] + max_total_joltage[i+1:12] + current_joltage
+            if int(temp) > int(max_total_joltage):
+                max_total_joltage = temp
+                break
+    return int(max_total_joltage)
+
+
 # Open the file in read mode
-file = open("input-sample.txt", "r")
+file = open("input-full.txt", "r")
 
 # iterate though file
 total_joltage = 0
 for bank in file:
-    max_joltage = find_max_joltage_bank_part_1(bank)
-    # print(f"Bank {bank}: max joltage = {max_joltage}")
+    max_joltage = find_max_joltage_bank_part_2_optimized(bank)
     total_joltage += max_joltage
 
 print(total_joltage)
